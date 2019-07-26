85daa76f3aebeb31902a5cfbf2562d48c7defeda
ShristySk1 <srezty@gmail.com>
2019-07-27 01:19:24 +0545

    initial commit

---
+++ src/main/java/com/shristy/perfect/base/HomeController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.shristy.perfect.base;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ *
+ * @author Administrator
+ */
+@Controller
+@RequestMapping(value = "/")
+public class HomeController {
+    public @ResponseBody String print(){
+        return "hello";
+    }
+}
